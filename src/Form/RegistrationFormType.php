e84d8e6438f8bd2eed70290621220ac1313b9683
Clément Broucke <cbroucke@yahoo.com>
2019-11-27 15:02:33 +0100

    meilleur registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,28 +4,30 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('email', TextType::class, [
+                'attr'=> [
+                    
+                    'class'=> "input--style-4"
+                ]
             ])
+            
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -40,7 +42,83 @@ class RegistrationFormType extends AbstractType
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                ],  'label' => 'Mot de passe',
+                    'attr' => [
+                        
+                        'class' => "input--style-4"
+                    ]
+                
+            ])
+            ->add('name', TextType::class,[
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('first_name', TextType::class,[
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('adresse', TextType::class,[
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('ville', TextType::class,[
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('pays', TextType::class,[
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('codePostal', IntegerType::class,[
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('formeJuridique', ChoiceType::class,[
+                'attr' => [
+                    'class' => "input--style-4"
+                    
                 ],
+                'choices' => [
+                    'Choisissez une option' => null,
+                    'Particulier' => 'particulier',
+                    'Entreprise' => 'entreprise'
+                ]
+            ])
+                
+            ->add('societyName', TextType::class , [
+                'label' => 'Nom de la société' ,
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('numero_tel',TextType::class , [
+                'label' => 'Numéro de téléphone' ,
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('num_siret',TextType::class , [
+                'label' => 'Numéro de SIRET' ,
+                'attr' => [
+                    'class' => "input--style-4"
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                  
+                ],'label' => false
             ])
         ;
     }
