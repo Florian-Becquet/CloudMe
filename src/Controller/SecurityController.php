e688d8773bbf5d24fb6963f2e8c617a77fc75ba4
Slimane Othmani <slimaneothmani@gmail.com>
2019-11-27 15:23:43 +0100

    Lien vers home

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,4 +33,12 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(){
+
+        return $this->render('pages/home.html.twig');
+    }
 }
