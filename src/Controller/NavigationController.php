990a1708f1a2cf1718cb36dfd38c684529ef3bd1
Clément Broucke <cbroucke@yahoo.com>
2019-12-02 16:25:36 +0100

    import Service dans Controller

--- src/Controller/NavigationController.php
+++ src/Controller/NavigationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Services;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class NavigationController extends AbstractController
 {
