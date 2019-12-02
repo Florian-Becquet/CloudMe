ae2c79fc9dc9408a479d95ebf703f6afc2ac5648
unknown <shiynwow90@gmail.com>
2019-12-02 13:38:37 +0100

    debut ajax

--- public/js/global.js
+++ public/js/global.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    
+    // chargement de la page home par defaut quand le document est pret dans la div root dans base twig
     $('#root').load('intro');
 
+    // chargement ajax des autres pages sur le click de chaques sections differentes
     $('#createServe').on('click',function(){
         $('#root').load('serveur');
     })
+
+    //
 })
 
 
