cc9c2e09941e67961894377c862fbae0e8a8d68f
unknown <shiynwow90@gmail.com>
2019-12-10 21:07:12 +0100

    reglage bug chargmeent de serveur deux fois

--- public/js/global.js
+++ public/js/global.js
@@ -29,7 +29,18 @@ $(document).ready(function(){
     })
     //
 
-
+    $('.subscription').on('click',function(){
+        
+        var target = $(this).data('target');
+        $.ajax({
+          type: 'POST',
+          url: 'serveur',
+          data: 'serv=' + target ,
+          success: function(data){
+            $('#root').html(data);
+          }
+        })
+    })
 })
 
 
