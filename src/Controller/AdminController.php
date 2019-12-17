9fa3eeccf26d7c329bd0d39a73974a943df9bf07
Clément Broucke <cbroucke@yahoo.com>
2019-12-17 10:39:01 +0100

    liste user

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,11 +10,24 @@ class AdminController extends AbstractController
 {
     /**
      * @Route("/listUser", name="listUser")
+     * 
+     * controller pour l'affichage de la liste des utilisateures uniquement accesible pour l'admin
      */
     public function listUser(UserRepository $userRepo)
     {
+        $subscriptions = array();
+        $countSub = array();
+        $i=0;
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $listUser = $userRepo->findAll();
-        return $this->render('admin/listUser.html.twig', ['users' => $listUser]);
+        //boucle dans laquelle pour chaque utilisateur on va aller faire un compte de ses souscriptions et ensuite le set dans un tableau 
+        foreach ($listUser as $users){
+            $subscriptions[$i] = $users->getSubscriptions();
+            $countSub[$i] = count($subscriptions[$i]);
+            $allUser[$i] = ['name' => $users->getName(), 'firstName' => $users->getFirstName(), 'status' => $users->GetStatus(), 'email' => $users->getEmail(), 'count' => $countSub[$i]];
+            $i++;
+        }
+        
+        return $this->render('admin/listUser.html.twig', ['users' => $allUser]);
     }
 }
