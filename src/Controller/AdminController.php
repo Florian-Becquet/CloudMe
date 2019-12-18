f9eea36c352851ee873029a21699c669e58866fe
Clément Broucke <cbroucke@yahoo.com>
2019-12-18 10:36:12 +0100

    knp_paginator + listeserv et liste sub

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Repository\ServicesRepository;
+use App\Repository\SubscriptionRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,7 +24,7 @@ class AdminController extends AbstractController
         $i=0;
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $listUser = $userRepo->findAll();
-        //boucle dans laquelle pour chaque utilisateur on va aller faire un compte de ses souscriptions et ensuite le set dans un tableau 
+        //boucle dans laquelle pour chaque utilisateur on fait un compte de ses souscriptions et ensuite le set dans un tableau 
         foreach ($listUser as $users){
             $subscriptions[$i] = $users->getSubscriptions();
             $countSub[$i] = count($subscriptions[$i]);
@@ -29,5 +33,29 @@ class AdminController extends AbstractController
         }
         
         return $this->render('admin/listUser.html.twig', ['users' => $allUser]);
+    }
+    /**
+     * @Route("/listServ", name="listServ")
+     * 
+     * controller pour l'affichage de la liste des services uniquement accesible pour l'admin
+     */
+    public function listServ(ServicesRepository $serviceRepo, PaginatorInterface $paginator, Request $request){
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $listServ = $serviceRepo->findAll();
+        $services = $paginator->paginate(
+            $listServ, // Requête contenant les données à paginer (ici nos services)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            5 // Nombre de résultats par page
+        );
+        return $this->render('admin/listServ.html.twig', ['services' => $services]);
+    }
+    /**
+     * @Route("/listSub", name="listSub")
+     * 
+     * controller pour l'affichage de la liste des souscriptions uniquement accesible pour l'admin
+     */
+    public function listSub(SubscriptionRepository $subRepo){
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        return $this->render('admin/listSub.html.twig');
     }
 }
