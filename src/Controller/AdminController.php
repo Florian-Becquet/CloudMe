342610f3ca65e0a3de8b94b703522f58c4e04f14
Clément Broucke <cbroucke@yahoo.com>
2019-12-31 11:21:08 +0100

    ajout deny acc is not admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -288,6 +288,7 @@ class AdminController extends AbstractController
      * Désabonner un utilisateur par l'Admin
      */
     public function unSub(Request $request, SubscriptionRepository $subRepo,EntityManagerInterface $em){
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $id = $request->request->get('id');
         $sub = $subRepo->find($id);
         $dateUnSub = new DateTime();
